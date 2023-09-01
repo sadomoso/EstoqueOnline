9bce01e681dd00e6fe49e8d39687dd0f7e337bcc
felipe <Felipe.FerreiraCarvalho2000@gmail.com>
2023-09-01 10:44:00 -0300

    Page home v1 add appbar

---
+++ src/Components/Incon.module.js
@@ -0,0 +1,8 @@
+import { IconButton  } from "@mui/material";
+
+export default function IconsP()
+{
+    return(
+        <IconButton size="larger" edge='start' color='inherit'></IconButton >
+    )
+}
--- src/Paginas/Home.js
+++ src/Paginas/Home.js
@@ -1,7 +1,28 @@
-import React from "react"
+import React from "react";
+import { Box, Toolbar, IconButton, AppBar, Typography, Button } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { AccountCircle } from "@mui/icons-material";
 
 export default function Home() {
     return (
-        <>ola</>
-    )
+        <Box>
+            <AppBar sx={{ backgroundColor: 'black' }}>
+                <Toolbar>
+                    <IconButton size="larger" sx={{ p: 2 }} color="inherit">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography padding={2} variant='h3' display="inline-flex">Logo</Typography>
+                    <Typography marginTop={2} marginLeft={3} variant='h6' display="inline-flex">CADASTRAR</Typography>
+                    <Typography marginTop={2} marginLeft={3} variant='h6' display="inline-flex">SACAR</Typography>
+
+                    <Box sx={{ flexGrow: 1 }} /> {/* Espaço flexível para empurrar o botão para a direita */}
+                    <Box>
+                        <Typography padding={2} variant='h7' display="inline-flex">SALDO: R$</Typography>
+                        <Typography variant='h7' display="inline-flex">0,00</Typography>
+                    </Box>
+                    <Button color="inherit"><AccountCircle sx={{fontSize:'2rem'}} /></Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
