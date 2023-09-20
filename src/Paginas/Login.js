2d72d7a6e721ef7d577546079e5fdb42648daa08
felipe <Felipe.FerreiraCarvalho2000@gmail.com>
2023-09-20 09:18:25 -0300

    Creacao do consumo do backend e adaptação da resposta da pagina Login

--- src/Paginas/Login.js
+++ src/Paginas/Login.js
@@ -1,54 +1,68 @@
-import { Alert, Box } from "@mui/material"
-import ButtonP from "../Components/Button.module"
+import { Box } from "@mui/material";
+import ButtonP from "../Components/Button.module";
 import loginRequisicao from "../Requisicoes/Login.Requisicao";
 import React, { useState } from "react";
 import TextFieldP from "../Components/TextField.module";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const [usuario, setUsuario] = useState(""); // Estado para armazenar o usuário
-    const [senha, setSenha] = useState(""); // Estado para armazenar a senha
-    const navigate = useNavigate();
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = () => {
-        setUsuario(usuario)
-        setSenha(senha)
-        // Aqui você pode acessar os valores de usuário e senha
-        const loginsucesso = loginRequisicao(usuario, senha);
-        if (loginsucesso == true) {
-            navigate("/Home");
-        }
-        else {
-            alert('Login ou Senha incorretos')
-        }
+  const handleLogin = async () => {
+    try {
+      // Chama a função de requisição de login de forma assíncrona
+      const loginsucesso = await loginRequisicao(usuario, senha);
 
-        // Chama a função de requisição de login
-    };
+      if (loginsucesso === true) {
+        navigate("/Home");
+        console.log("chegou aqui");
+      } else {
+        alert("Usuário ou Senha incorretos");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao fazer login"); // Trate o erro de login de acordo com suas necessidades
+    }
+  };
 
-    return (
-        <Box display='flex' justifyContent='center' alignItems='center' height='100vh'>
-            <Box
-                component='form'
-                width='40vh'
-                height='300px'
-                p={2}
-                display='flex'
-                flexDirection='column'
-                alignItems='center'
-                justifyContent='center'
-                borderRadius={'10px'}
-                borderColor={'Grey'}
-                sx={{ backgroundColor: 'white' }}
-                border={'4px solid'}
-                boxShadow='0px 3px 6px rgba(0, 0, 0, 0.16)'
-            // Remova a margem superior da primeira Box
-            >
-                <h1>Loguin</h1>
-                <TextFieldP propsTexto={{ Texto: "Usuario" }} propsType={{ Type: 'text' }} propsOnChange={(e) => setUsuario(e.target.value)} ></TextFieldP>
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Box
+        component="form"
+        width="40vh"
+        height="300px"
+        p={2}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        borderRadius={"10px"}
+        borderColor={"Grey"}
+        sx={{ backgroundColor: "white" }}
+        border={"4px solid"}
+        boxShadow="0px 3px 6px rgba(0, 0, 0, 0.16)"
+      >
+        <h1>Login</h1>
+        <TextFieldP
+          propsTexto={{ Texto: "Usuario" }}
+          propsType={{ Type: "text" }}
+          propsOnChange={(e) => setUsuario(e.target.value)}
+        ></TextFieldP>
 
-                <TextFieldP propsTexto={{ Texto: 'Senha' }} propsType={{ Type: 'Password' }} propsOnChange={(e) => setSenha(e.target.value)}></TextFieldP>
-                <ButtonP Texto='Confirmar' propsFunciton={handleLogin} />
-            </Box>
-        </Box>
-    )
+        <TextFieldP
+          propsTexto={{ Texto: "Senha" }}
+          propsType={{ Type: "Password" }}
+          propsOnChange={(e) => setSenha(e.target.value)}
+        ></TextFieldP>
+        <ButtonP Texto="Confirmar" propsFunciton={handleLogin} />
+      </Box>
+    </Box>
+  );
 }
