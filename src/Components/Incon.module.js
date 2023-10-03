0f2d7085abb96378bc5b5325fc32f0c3bb0d96ee
felipe <Felipe.FerreiraCarvalho2000@gmail.com>
2023-10-03 14:02:45 -0300

    Exclusão dos componentes

--- src/Components/Incon.module.js
+++
@@ -1,8 +0,0 @@
-import { IconButton  } from "@mui/material";
-
-export default function IconsP()
-{
-    return(
-        <IconButton size="larger" edge='start' color='inherit'></IconButton >
-    )
-}
